fd5e10e00a0f2a5f18af9894d35224ace98b6721
Ola-jed <olabijed@gmail.com>
2025-12-08 07:26:42 +0100

    Day 8, initialized

---
+++ Aoc2025/Days/Day8.fs
@@ -0,0 +1,45 @@
+﻿module Aoc2025.Day8
+
+open Aoc2025.Days
+
+type JunctionBox = { X: int; Y: int; Z: int }
+
+type Circuit = JunctionBox list
+
+type Day8() =
+    let Distance x y =
+        (x.X - y.X) * (x.X - y.X)
+        + (x.Y - y.Y) * (x.Y - y.Y)
+        + (x.Z - y.Z) * (x.Z - y.Z)
+
+    let isInCircuit (box: JunctionBox) (circuits: JunctionBox list list) =
+        circuits |> List.filter (List.contains box)
+
+
+    interface IDay with
+        member _.Day = 8
+
+        member this.Part1() =
+            let boxes =
+                Input.readLines (this :> IDay).Day
+                |> Array.map _.Split(",")
+                |> Array.map (fun x ->
+                    { X = int (x[0])
+                      Y = int (x[1])
+                      Z = int (x[2]) })
+
+            let circuits = []
+            let pairsWithDistance =
+                [ for i in 0 .. boxes.Length - 1 do
+                      for j in i + 1 .. boxes.Length - 1 do
+                          let a = boxes[i]
+                          let b = boxes[j]
+                          let d = Distance a b
+                          yield (a, b, d) ]
+                |> List.sortBy (fun (_, _, d) -> d)
+
+
+            ""
+
+
+        member this.Part2() = ""
--- Aoc2025/Program.fs
+++ Aoc2025/Program.fs
@@ -1,9 +1,9 @@
-﻿open Aoc2025.Day7
+﻿open Aoc2025.Day8
 open Aoc2025.Days
 
 [<EntryPoint>]
 let main _ =
-    let day = Day7() :> IDay
+    let day = Day8() :> IDay
     printfn $"%s{day.Part1()}"
     printfn $"%s{day.Part2()}"
 
