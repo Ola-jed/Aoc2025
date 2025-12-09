020281b5692166555f4455a4d523d86c13015548
Ola-jed <olabijed@gmail.com>
2025-12-09 20:39:49 +0100

    Day 9, part 1

--- Aoc2025/Days/Day9.fs
+++ Aoc2025/Days/Day9.fs
@@ -32,19 +32,18 @@ type Day9() =
         else
             let topmost = if a.X < b.X then a else b
             let bottommost = if a.X < b.X then b else a
-            
-            if topmost.Y < bottommost.Y then // topmost is top left, bottommost is bottom right
+
+            if topmost.Y < bottommost.Y then
                 { TL = topmost
-                  BL = {X = bottommost.X; Y = topmost.Y}
-                  TR = {X = topmost.X; Y = bottommost.Y}
+                  BL = { X = bottommost.X; Y = topmost.Y }
+                  TR = { X = topmost.X; Y = bottommost.Y }
                   BR = bottommost }
-            else // TODO  // topmost is top right, bottommost is bottom left
-                { TL = topmost
+            else
+                { TL = { X = topmost.X; Y = bottommost.Y }
                   BL = bottommost
                   TR = topmost
-                  BR = bottommost }
-            
-            
+                  BR = { X = bottommost.X; Y = topmost.Y } }
+
 
     let area rectangle =
         if rectangle.TL = rectangle.BL then
@@ -52,7 +51,7 @@ type Day9() =
         else if rectangle.TL = rectangle.TR then
             rectangle.BL.Y - rectangle.TL.X
         else
-            (rectangle.TR.Y - rectangle.TL.Y) * (rectangle.BL.X - rectangle.TL.X)
+            (rectangle.TR.Y - rectangle.TL.Y + 1L) * (rectangle.BL.X - rectangle.TL.X + 1L)
 
     interface IDay with
         member _.Day = 9
@@ -70,12 +69,8 @@ type Day9() =
                   for j in i + 1 .. redTiles.Length - 1 do
                       let a, b = redTiles[i], redTiles[j]
                       yield a, b ]
-            |> List.map (fun (a, b) ->
-                let topLeft = if a.X < b.X && a.Y < b.Y then a else b
-                let bottomRight = if a.X < b.X && a.Y < b.Y then b else a
-                let topRight = { X = topLeft.X; Y = bottomRight.Y }
-                let bottomLeft = { X = bottomRight.X; Y = topLeft.Y }
-                (topRight.Y - topLeft.Y) * (bottomLeft.X - topLeft.X))
+            |> List.map (fun (a, b) -> inferRectangle a b)
+            |> List.map area
             |> List.max
             |> string
 
