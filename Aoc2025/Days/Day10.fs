5fc274cc3aba1e8eeab8204cfeac3deca4d7a444
Ola-jed <olabijed@gmail.com>
2025-12-10 06:59:30 +0100

    Day 10, init

---
+++ Aoc2025/Days/Day10.fs
@@ -0,0 +1,42 @@
+﻿module Aoc2025.Day10
+
+open System.Text.RegularExpressions
+open Aoc2025.Days
+
+type Machine =
+    { LightDiagram: int
+      Buttons: int array array
+      JoltageRequirements: int array }
+
+type Day10() =
+    let setBit number position = number ||| (1 <<< position)
+
+    let unsetBit number position = number &&& (~~~(1 <<< position))
+
+    let parseLine str =
+        let lightDiagram =
+            Regex.Match(str, @"\[(.*?)\]").Groups.[1].Value.ToCharArray()
+            |> Seq.indexed
+            |> Seq.fold (fun acc (i, c) -> if c = '#' then setBit acc i else acc) 0
+
+        let buttons =
+            Regex.Matches(str, @"\((.*?)\)")
+            |> Seq.cast<Match>
+            |> Seq.map (fun m -> m.Groups.[1].Value.Split(',') |> Array.map int)
+            |> Seq.toArray
+
+        let joltageRequirements =
+            Regex.Match(str, @"\{(.*?)\}").Groups.[1].Value.Split(",")
+            |> Array.map (fun x -> (int (x) - int ('0')))
+
+        { LightDiagram = lightDiagram
+          Buttons = buttons
+          JoltageRequirements = joltageRequirements }
+
+    interface IDay with
+        member _.Day = 10
+
+        member this.Part1() = ""
+
+
+        member this.Part2() = ""
--- Aoc2025/Program.fs
+++ Aoc2025/Program.fs
@@ -1,9 +1,9 @@
-﻿open Aoc2025.Day9
+﻿open Aoc2025.Day10
 open Aoc2025.Days
 
 [<EntryPoint>]
 let main _ =
-    let day = Day9() :> IDay
+    let day = Day10() :> IDay
     printfn $"%s{day.Part1()}"
     printfn $"%s{day.Part2()}"
 
