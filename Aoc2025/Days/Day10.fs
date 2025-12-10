70ae694b9d12a48fdb5b5f90d584dd9b1701d33f
Ola-jed <olabijed@gmail.com>
2025-12-10 22:16:18 +0100

    Day 10, part 2 feat OrTools

--- Aoc2025/Days/Day10.fs
+++ Aoc2025/Days/Day10.fs
@@ -1,8 +1,10 @@
 ﻿module Aoc2025.Day10
 
 open System.Collections.Generic
+open System.Linq
 open System.Text.RegularExpressions
 open Aoc2025.Days
+open Google.OrTools.LinearSolver
 
 type Machine =
     { LightDiagram: int
@@ -17,7 +19,7 @@ type Day10() =
     let toggleBit number position = number ^^^ (1 <<< position)
 
     let applyButton button number = button |> Array.fold toggleBit number
-    
+
     let parseLine str =
         let lightDiagram =
             Regex.Match(str, @"\[(.*?)\]").Groups[1].Value.ToCharArray()
@@ -32,36 +34,62 @@ type Day10() =
 
         let joltageRequirements =
             Regex.Match(str, @"\{(.*?)\}").Groups[1].Value.Split(",")
-            |> Array.map (fun x -> (int x - int '0'))
+            |> Array.map int
 
         { LightDiagram = lightDiagram
           Buttons = buttons
           JoltageRequirements = joltageRequirements }
-        
+
     let bfs (target: int) (buttons: int array array) =
         let visited = HashSet<int>()
         let queue = Queue<int * int>()
-        
+
         // state, button presses
         queue.Enqueue((0, 0))
         visited.Add 0 |> ignore
         let mutable buttonPresses = 9999999
+
         while queue.Count > 0 do
             let state, presses = queue.Dequeue()
-            
+
             if state = target then
                 buttonPresses <- min buttonPresses presses
-            
+
             for button in buttons do
                 let temp = applyButton button state
-                
+
                 if visited.Contains(temp) = false then
                     visited.Add temp |> ignore
                     queue.Enqueue((temp, presses + 1))
-            
+
         buttonPresses
-        
-    
+
+
+    let modelIp machine =
+        let solver = Solver.CreateSolver("SCIP")
+
+        let variables =
+            machine.Buttons
+            |> Array.map (fun i -> solver.MakeIntVar(0.0, infinity, $"x_{i}"))
+
+        for joltageIdx in 0 .. machine.JoltageRequirements.Length - 1 do
+            let joltageRequirement = machine.JoltageRequirements[joltageIdx]
+            let cons = solver.MakeConstraint(joltageRequirement, joltageRequirement)
+
+            for index in 0 .. machine.Buttons.Length - 1 do
+                let button = machine.Buttons[index]
+                let variable = variables[index]
+                let coefficient = if button.Contains(joltageIdx) then 1 else 0
+                cons.SetCoefficient(variable, coefficient)
+
+
+        let objective = solver.Objective()
+        for variable in variables do
+            objective.SetCoefficient(variable, 1)
+
+        objective.SetMinimization()
+        solver
+
 
     interface IDay with
         member _.Day = 10
@@ -69,9 +97,17 @@ type Day10() =
         member this.Part1() =
             Input.readLines (this :> IDay).Day
             |> Array.map parseLine
-            |> Array.map(fun x -> bfs x.LightDiagram x.Buttons)
+            |> Array.map (fun x -> bfs x.LightDiagram x.Buttons)
             |> Array.sum
             |> string
 
 
-        member this.Part2() = ""
+        member this.Part2() =
+            Input.readLines (this :> IDay).Day
+            |> Array.map parseLine
+            |> Array.map modelIp
+            |> Array.map (fun x ->
+                x.Solve() |> ignore
+                x.Objective().Value())
+            |> Array.sum
+            |> string
